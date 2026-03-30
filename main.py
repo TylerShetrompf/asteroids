fecd916ebcf1c8bdd93a0824a24485741d76d89f
TylerShetrompf <tyler@shetrompf.com>
2026-03-30 16:21:20 -0400

    Corrected print lines in main.py

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import pygame
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
+    print(f"Screen Width: {SCREEN_WIDTH}")
+    print(f"Screen Height: {SCREEN_HEIGHT}")
+
 
 
 if __name__ == "__main__":
