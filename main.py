53d49f01c92036df5281729be0b660332116facb
TylerShetrompf <tyler@shetrompf.com>
2026-04-01 11:31:47 -0400

    added asteroid destruction

--- main.py
+++ main.py
@@ -47,7 +47,13 @@ def main():
                 log_event("player_hit")
                 print("Game over!")
                 sys.exit()
-        
+
+            for shot in shots:
+                if asteroid.collides_with(shot):
+                    log_event("asteroid_shot")
+                    shot.kill()
+                    asteroid.kill()
+
         for game_object in drawable:
             game_object.draw(screen)
         
